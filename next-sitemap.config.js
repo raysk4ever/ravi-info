be7f925918cc339bc9bf6a6312855965b31db8d8
Ravi Singh <raysk7161@gmail.com>
2024-05-26 15:10:25 +0530

    added next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,44 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://www.socialamigo.in', // Replace with your site's URL
+  generateRobotsTxt: true, // (Optional) Generate robots.txt file
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/admin/*', '/login', '/register'], // Add any paths you want to exclude
+  // Additional options...
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, 'https://budget.socialamigo.in'),
+      // Add more static pages here as needed
+    ]
+  },
+  // for custom dynamic paths
+  // transform: async (config, path) => {
+  //   // Default transformation
+  //   let transformed = {
+  //     loc: path, // The url of the page
+  //     changefreq: config.changefreq,
+  //     priority: config.priority,
+  //     lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+  //     alternateRefs: config.alternateRefs ?? [],
+  //   };
+
+  //   // Add custom logic for dynamic routes
+  //   if (path === '/blog/[slug]') {
+  //     const response = await fetch('https://api.yourdomain.com/posts'); // Replace with your API
+  //     const posts = await response.json();
+
+  //     return posts.map(post => ({
+  //       loc: `${config.siteUrl}/blog/${post.slug}`,
+  //       changefreq: 'daily',
+  //       priority: 0.7,
+  //       lastmod: post.updatedAt, // Assuming each post has an updatedAt field
+  //     }));
+  //   }
+
+  //   return transformed;
+  // }
+}
+
+module.exports = config;
