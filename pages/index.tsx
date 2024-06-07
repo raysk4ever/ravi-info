1204e70f65fa49fe74ea112a147ce4d9fcaba9a0
Ravi <Ravikant.Singh@externals.adidas.com>
2024-06-07 21:15:47 +0530

    updated meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <>
       <Head>
         <title>Ravi Singh | Software Engineer</title>
-        <meta name="description" content="Ravi Singh | Software Engineer" />
-        <meta name="keywords" content="ravi, ravi kant singh, ravi singh, raysk4ever, rudrapur, ravi kant singh rudrapur, bareilly software engineer, teasit, kimblytech, ray, React, Nodejs, Docker, Nextjs, Developer, Engineer, Typscript" />
+        <meta name="description" content="Experienced software engineer specializing in JavaScript frameworks such as React, Angular, and Vue.js. Skilled in full-stack and mobile app development using Node.js and modern technologies. Available for hire for custom software development and comprehensive technology solutions. Explore my portfolio and contact me for professional software development services." />
+        <meta name="keywords" content="Software Engineer, JavaScript Developer, Full-Stack Developer, Front-End Developer, Back-End Developer, JavaScript Frameworks, React Developer, Angular Developer, Vue.js Developer, Node.js Developer, Web Developer, Mobile App Developer, Custom Software Development, Hire Software Engineer, Software Development Services, Professional Developer, Freelance Developer, Technology Expert, Web Application Development, Mobile Application Development, Full-Stack Development, Modern Tech Solutions, Freelance Software Engineer, Software Engineering Services, Software Developer for Hire, ravi, ravi kant singh, ravi singh, raysk4ever, rudrapur, ravi kant singh rudrapur, bareilly software engineer, teasit, kimblytech, ray, React, Nodejs, Docker, Nextjs, Developer, Engineer, Typscript" />
         <meta name="author" content="Ravi Singh" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/cloud.png" />
