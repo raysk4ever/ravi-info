c354af4b4b66e09b031c9114b468086cf46f98bc
Ravi Singh <raysk7161@gmail.com>
2024-06-23 00:30:54 +0530

    aaded pp

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,38 @@
+const PP = () => {
+  return ( 
+    <div>
+      <h1>
+        Privacy Policy
+      </h1>
+        Effective Date: 23 June 2024
+
+        <p>1. Introduction</p>
+        We respect your privacy and are committed to protecting your personal information. This privacy policy explains how we handle your data when you use our app.
+
+        <p>2. Information We Collect</p>
+        We do not collect or store any personal information on our servers. All data, including contact information, is stored locally on your device.
+
+        <p>3. How We Use Your Information</p>
+
+        Quick Calls: To enable you to make quick calls to your favorite contacts stored on your device.
+        <p>4. Data Sharing and Disclosure</p>
+        We do not share, sell, or transmit any of your personal information to third parties. All data remains on your local device.
+
+        <p>5. Data Security</p>
+        We rely on your device's security measures to protect your data. We recommend keeping your device secure to prevent unauthorized access.
+
+        <p>6. Children's Privacy</p>
+        Our app is not intended for children under 13. We do not knowingly collect or store data from children under 13.
+
+       <p> 7. Changes to This Privacy Policy</p>
+        We may update our privacy policy from time to time. Any changes will be posted on this page.
+
+        <p>8. Contact Us</p>
+        If you have any questions or concerns about this privacy policy, please contact us at [Your Contact Information].
+
+        By using our app, you agree to the terms outlined in this privacy policy.
+    </div>
+  );
+}
+ 
+export default PP;
