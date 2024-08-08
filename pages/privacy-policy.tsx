72aff140ab05f9c8f6d03e4818f6ba2d17abc562
Ravi <Ravikant.Singh@externals.adidas.com>
2024-08-08 15:09:36 +0530

    fixed pp

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,31 +1,35 @@
 const PP = () => {
   return ( 
-    <div>
-      <h1>Privacy Policy for Emergency Call INDIA</h1>
-    <p>Effective Date: July 4, 2024</p>
-    
+    <>
+    <h1>Privacy Policy for Emergency Call INDIA</h1>
+    <p><strong>Effective Date:</strong> July 4, 2024</p>
+
     <h2>1. Introduction</h2>
-    <p>Welcome to Emergency Call INDIA (&quot;we,&quot; &quot;our,&quot; &quot;us&quot;). We respect your privacy and are committed to protecting the personal information you may provide us through our application (&quot;App&quot;). This Privacy Policy explains our practices regarding the collection, use, and disclosure of your information when you use our App.</p>
-    
+    <p>Welcome to Emergency Call INDIA ("we," "our," "us"). We respect your privacy and are committed to protecting any personal information you may provide through our application ("App"). This Privacy Policy outlines our practices regarding the collection, use, and disclosure of your information when you use our App.</p>
+
     <h2>2. Information We Collect</h2>
     <h3>Personal Data:</h3>
-    <p>We do not collect any personal data through the Emergency Call INDIA app.</p>
-    <h3>Non-Personal Data:</h3>
-    <p>The app does not collect any non-personal data.</p>
-    
-    <h2>3. Use of Your Information</h2>
-    <p>Since we do not collect any personal or non-personal data, there is no information to use for any purpose.</p>
-    
-    <h2>4. Data Security</h2>
-    <p>We do not collect, store, or process any user data. Therefore, we do not have any specific data security measures in place for user data. The app operates entirely on your device without any data being transmitted to us or third parties.</p>
-    
-    <h2>5. Children&apos;s Privacy</h2>
-    <p>Our app does not knowingly collect any personal information from children under the age of 13. If we become aware that a child under 13 has provided us with personal information, we will take steps to delete such information.</p>
-    
-    <h2>6. Changes to This Privacy Policy</h2>
-    <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.</p>
+    <p><strong>No Collection:</strong> We do not collect any personal data through the Emergency Call INDIA app.</p>
 
-    </div>
+    <h3>Non-Personal Data:</h3>
+    <p><strong>No Collection:</strong> The app does not collect any non-personal data.</p>
+
+    <h2>3. Use of Your Information</h2>
+    <p><strong>No Use:</strong> Since we do not collect any personal or non-personal data, there is no information to use for any purpose.</p>
+
+    <h2>4. Data Security</h2>
+    <p><strong>No Data Collected:</strong> We do not collect, store, or process any user data.</p>
+    <p><strong>Security Measures:</strong> As there is no data transmission to us or third parties, specific data security measures are not necessary. The app operates entirely on your device.</p>
+
+    <h2>5. Children's Privacy</h2>
+    <p><strong>No Collection from Children:</strong> Our app does not knowingly collect any personal information from children under the age of 13.</p>
+    <p><strong>Actions Upon Awareness:</strong> If we become aware that a child under 13 has provided us with personal information, we will take immediate steps to delete such information.</p>
+
+    <h2>6. Changes to This Privacy Policy</h2>
+    <p><strong>Policy Updates:</strong> We may update our Privacy Policy from time to time.</p>
+    <p><strong>Notification of Changes:</strong> We will notify you of any changes by posting the updated Privacy Policy on this page.</p>
+    <p><strong>Reviewing the Policy:</strong> You are encouraged to review this Privacy Policy periodically for any updates or changes.</p>
+    </>
   );
 }
  
