a4a85e8c66500b3ecbfeae1f64a5946660d271b6
Ravi Singh <raysk7161@gmail.com>
2025-12-31 01:10:17 +0530

    fixed cologs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,12 @@ export default function Document() {
       <Head>
         <meta property="og:site_name" content="Ravi Singh Portfolio" />
         <meta name="twitter:site" content="@raysk4ever" />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="Ravi Singh Blog RSS Feed"
+            href="/rss.xml"
+          />
       </Head>
       <body>
         <Main />
